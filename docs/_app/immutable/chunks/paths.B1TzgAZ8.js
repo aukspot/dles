10fb9ff4f9b445118fc3c5acb023ea4213c75b7b
aukspot <aukspot@proton.me>
2024-02-17 20:22:56 -0500

    Add 8 dles.

---
+++ docs/_app/immutable/chunks/paths.B1TzgAZ8.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1bf7lzk)==null?void 0:s.base)??"/dles";var e;const l=((e=globalThis.__sveltekit_1bf7lzk)==null?void 0:e.assets)??a;export{l as a,a as b};
---
+++ docs/_app/immutable/entry/start.AzikWamV.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.CanFbgeq.js";export{t as start};
