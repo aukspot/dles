a7936c27f12d7e51f880a48a75f133937c692843
aukspot <aukspot@proton.me>
2024-05-26 17:08:40 -0400

    Begin reformatting footer

--- src/lib/components/LatestChange.svelte
+++ src/lib/components/LatestChange.svelte
@@ -1,5 +1,16 @@
 <script>
+  import { base } from "$app/paths"
   import changelog from "$lib/data/changelog.json"
 
   const latestChange = changelog[0]
 </script>
+
+<div class="mt-2">
+  <h2 class="text-center text-lg underline underline-offset-2">
+    Latest changes ({latestChange.date})
+  </h2>
+  <div class="text-sm flex justify-center">
+    {@html latestChange.description}
+  </div>
+  <a class="flex justify-center" href="{base}/changelog">View full changelog</a>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -105,20 +105,23 @@
     </main>
     <footer class="flex flex-col p-1">
       <div class="divider"></div>
-      <div data-sveltekit-reload>
-        <a class="menu-link btn-menu-item" href="{base}/suggest"
-          ><IconSuggest /> Suggest a game</a
-        >
-        <a class="menu-link btn-menu-item" href="{base}/report"
-          ><IconBug /> Report a bug</a
-        >
-        <a
-          class="menu-link btn-menu-item"
-          href="https://ko-fi.com/aukspot"
-          target="_blank"><IconKofi /> Support on Ko-fi</a
-        >
+      <div class="flex flex-col md:flex-row justify-around">
+        <LatestChange />
+
+        <div data-sveltekit-reload>
+          <a class="menu-link btn-menu-item" href="{base}/suggest"
+            ><IconSuggest /> Suggest a game</a
+          >
+          <a class="menu-link btn-menu-item" href="{base}/report"
+            ><IconBug /> Report a bug</a
+          >
+          <a
+            class="menu-link btn-menu-item"
+            href="https://ko-fi.com/aukspot"
+            target="_blank"><IconKofi /> Support on Ko-fi</a
+          >
+        </div>
       </div>
-      <LatestChange />
     </footer>
   </div>
 </div>
