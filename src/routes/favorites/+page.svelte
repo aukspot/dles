fa0970ade22f67a9d6c8f212364b8a363e32aaae
aukspot <aukspot@proton.me>
2024-06-01 01:29:48 -0400

    Add favorites page

---
+++ src/routes/favorites/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import DleFavorites from "$lib/components/Dles/DleFavorites.svelte"
+</script>
+
+<svelte:head>
+  <title>Your favorite dles!</title>
+  <meta
+    name="description"
+    content="Suggest a daily game to be added to the list of dles."
+  />
+</svelte:head>
+
+<DleFavorites />
