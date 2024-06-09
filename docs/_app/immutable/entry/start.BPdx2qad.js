0ead585da0cf4edd17ca8305eebc6ed1351a1dd1
aukspot <aukspot@proton.me>
2024-06-08 22:51:45 -0400

    Add sitemap.xml

---
+++ docs/_app/immutable/entry/start.BPdx2qad.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.DG8Hwoi9.js";export{t as start};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,5 +13,14 @@
     content="list, collection, links, games, daily, dles, wordle, timeguessr, bandle, connections, gamedle, like wordle"
   />
   <meta name="author" content="aukspot" />
+  <meta property="og:title" content="The DLES" />
+  <meta property="og:site_name" content="The DLES" />
+  <meta property="og:url" content="https://dles.aukspot.com" />
+  <meta property="og:type" content="website" />
+  <meta property="og:locale" content="en_US" />
+  <meta
+    property="og:description"
+    content="A curated collection of dles, or free daily games. Contains 200+ games - like Wordle, Connections, Bandle, and many more."
+  />
 </svelte:head>
 <DleList />
