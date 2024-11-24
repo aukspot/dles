062bc76293f3ed7403e64980122697253258cbcb
aukspot <aukspot@proton.me>
2024-11-24 00:02:43 -0500

    Add Airport Guessr

--- src/lib/components/BannerText.svelte
+++ src/lib/components/BannerText.svelte
@@ -3,6 +3,6 @@
 </script>
 
 <p class="p-3 text-center text-sm md:p-2 md:text-base">
-  <strong>Latest update</strong>: Added 34 new dles, look for the <IconNew /> tag
+  <strong>Latest update</strong>: Added 35 new dles, look for the <IconNew /> tag
   to find them!
 </p>
