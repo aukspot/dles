32b5f6b10367263c51a2f33fa9a2f4c3de99ba3e
aukspot <aukspot@proton.me>
2025-06-24 18:27:24 -0400

    Add 6 new categories

--- src/lib/js/categoryIcons.js
+++ src/lib/js/categoryIcons.js
@@ -1,22 +1,34 @@
+import IconCardBoardGames from "$lib/components/Icons/IconCardBoardGames.svelte"
+import IconColors from "$lib/components/Icons/IconColors.svelte"
+import IconFood from "$lib/components/Icons/IconFood.svelte"
 import IconGeography from "$lib/components/Icons/IconGeography.svelte"
 import IconMath from "$lib/components/Icons/IconMath.svelte"
 import IconMovies from "$lib/components/Icons/IconMovies.svelte"
 import IconMusic from "$lib/components/Icons/IconMusic.svelte"
 import IconPrices from "$lib/components/Icons/IconPrices.svelte"
+import IconScienceNature from "$lib/components/Icons/IconScienceNature.svelte"
+import IconShapesPatterns from "$lib/components/Icons/IconShapesPatterns.svelte"
 import IconSports from "$lib/components/Icons/IconSports.svelte"
 import IconTrivia from "$lib/components/Icons/IconTrivia.svelte"
+import IconVehicles from "$lib/components/Icons/IconVehicles.svelte"
 import IconVideoGames from "$lib/components/Icons/IconVideoGames.svelte"
 import IconWords from "$lib/components/Icons/IconWords.svelte"
 import IconMiscellaneous from "$lib/components/Icons/IconMiscellaneous.svelte"
 
 export const categoryIcons = {
+  "Card/Board Games": IconCardBoardGames,
+  Colors: IconColors,
+  Food: IconFood,
   "Geography/History": IconGeography,
   "Math/Logic": IconMath,
   "Movies/TV": IconMovies,
   Music: IconMusic,
   Prices: IconPrices,
+  "Science/Nature": IconScienceNature,
+  "Shapes/Patterns": IconShapesPatterns,
   Sports: IconSports,
   Trivia: IconTrivia,
+  Vehicles: IconVehicles,
   "Video Games": IconVideoGames,
   Words: IconWords,
   Miscellaneous: IconMiscellaneous,
