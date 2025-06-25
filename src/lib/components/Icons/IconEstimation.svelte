97d3fbdebaecb747b277d385d90500f5c01f0ee0
aukspot <aukspot@proton.me>
2025-06-24 21:12:36 -0400

    Add category Estimation, remove category Prices

---
+++ src/lib/components/Icons/IconEstimation.svelte
@@ -0,0 +1,13 @@
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="28"
+  fill="none"
+  stroke="currentColor"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  stroke-width="2"
+  viewBox="0 0 24 24"
+  ><path
+    d="m16 16 3-8 3.001 8A5.002 5.002 0 0 1 16 16zM2 16l3-8 3.001 8A5.002 5.002 0 0 1 2 16zM7 21h10M12 3v18M3 7h2c2 0 5-1 7-2 2 1 5 2 7 2h2"
+  /></svg
+>
--- src/lib/js/categoryIcons.js
+++ src/lib/js/categoryIcons.js
@@ -1,11 +1,11 @@
 import IconCardBoardGames from "$lib/components/Icons/IconCardBoardGames.svelte"
 import IconColors from "$lib/components/Icons/IconColors.svelte"
+import IconEstimation from "$lib/components/Icons/IconEstimation.svelte"
 import IconFood from "$lib/components/Icons/IconFood.svelte"
 import IconGeography from "$lib/components/Icons/IconGeography.svelte"
 import IconMath from "$lib/components/Icons/IconMath.svelte"
 import IconMovies from "$lib/components/Icons/IconMovies.svelte"
 import IconMusic from "$lib/components/Icons/IconMusic.svelte"
-import IconPrices from "$lib/components/Icons/IconPrices.svelte"
 import IconScienceNature from "$lib/components/Icons/IconScienceNature.svelte"
 import IconShapesPatterns from "$lib/components/Icons/IconShapesPatterns.svelte"
 import IconSports from "$lib/components/Icons/IconSports.svelte"
@@ -18,12 +18,12 @@ import IconMiscellaneous from "$lib/components/Icons/IconMiscellaneous.svelte"
 export const categoryIcons = {
   "Card/Board Games": IconCardBoardGames,
   Colors: IconColors,
+  Estimation: IconEstimation,
   Food: IconFood,
   "Geography/History": IconGeography,
   "Math/Logic": IconMath,
   "Movies/TV": IconMovies,
   Music: IconMusic,
-  Prices: IconPrices,
   "Science/Nature": IconScienceNature,
   "Shapes/Patterns": IconShapesPatterns,
   Sports: IconSports,
