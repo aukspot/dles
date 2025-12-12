c5ec860d90e438f11a45e823fab0b81ba77c7efc
aukspot <aukspot@proton.me>
2025-12-11 21:57:11 -0500

    Fix favorites page scrolling bug while reordering

--- src/lib/components/Dles/DleFavorites.svelte
+++ src/lib/components/Dles/DleFavorites.svelte
@@ -173,7 +173,7 @@
     <div class="m-auto px-2 pb-4">
       <div
         data-sveltekit-reload
-        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2"
+        class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 px-6"
         use:dndzone={{
           items,
           flipDurationMs: 200,
